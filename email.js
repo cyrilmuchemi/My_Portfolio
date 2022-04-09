40b1a3702aa7ba8b350c15ea2e8b5ce4ab8420ea
afikanzoro <muchemimutahi@gmail.com>
2022-04-09 09:56:05 +0300

    Made required changes

--- email.js
+++ email.js
@@ -3,11 +3,11 @@ const errorMessage = document.getElementById('errorDisplay');
 const errMsg = 'Please type your email in lower case';
 
 contactForm.addEventListener('submit', (event) => {
-  event.preventDefault();
   const email = document.getElementById('email').value;
   email.trim();
   if (email !== email.toLowerCase()) {
     errorMessage.textContent = errMsg;
+    event.preventDefault();
   } else {
     contactForm.submit();
     contactForm.reset();
